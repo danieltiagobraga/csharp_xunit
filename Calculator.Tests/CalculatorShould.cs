273baf116aff6f7b47604339ad91cdbf4d829557
Daniel Braga <daniel.braga@outsystems.com>
2021-12-29 23:44:51 +0000

    Add first test

--- Calculator.Tests/CalculatorShould.cs
+++ Calculator.Tests/CalculatorShould.cs
@@ -9,7 +9,22 @@ namespace Calculator.Tests
         [Fact]
         public void TestSum2And2()
         {
+            // Arrange Phase: 
+            // Set things up
+            // Create object instance
+            // Create test data/inputs)
             Calculator c = new Calculator();
+
+            // Act Phase:
+            // Execute production code
+            // Call methods
+            // Set properties
+            var sum = c.Sum(2, 2);
+
+            // Assert Phase 
+            // Check results 
+            // Tests passes/fails
+            Assert.Equal(4, sum);
         }
     }
 }
