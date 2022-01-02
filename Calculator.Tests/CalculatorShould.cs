818a5564eb8b7f5fd8f9ebc3ba96d1ff526f403c
Daniel Braga <daniel.braga@outsystems.com>
2022-01-02 21:51:56 +0000

    Add trait

--- Calculator.Tests/CalculatorShould.cs
+++ Calculator.Tests/CalculatorShould.cs
@@ -1,30 +1,78 @@
-using System;
+/*
+    Unit tests have normally 3 phases:
+
+        Arrange phase:
+            Set things up
+            Create object instance
+            Create test data/inputs
+
+        Act phase
+            Execute production code
+            Call methods
+            Set properties
+
+        Assert phase
+            Check results 
+            Tests passes/fails
+
+    
+    Command line execute: dotnet test --filter Category=Sum
+*/
 using Xunit;
-using Calculator;
+using Xunit.Abstractions;
 
 namespace Calculator.Tests
 {
     public class CalculatorShould
     {
-        [Fact]
+        private readonly ITestOutputHelper _output; // To write custom test output messages
+        private readonly Calculator c;
+
+        public CalculatorShould(ITestOutputHelper output, Calculator calculator) 
+        {
+            _output = output;
+            c = calculator;
+        }
+
+        [Fact]                                      // Indicates this method is a unit test
+        [Trait("Category", "Sum")]                  // Create a new category 
         public void TestSum2And2()
         {
-            // Arrange Phase: 
-            // Set things up
-            // Create object instance
-            // Create test data/inputs)
-            Calculator c = new Calculator();
+            // Arrange Phase
+            //Calculator c = new();
 
-            // Act Phase:
-            // Execute production code
-            // Call methods
-            // Set properties
+            // Act Phase
             var sum = c.Sum(2, 2);
 
             // Assert Phase 
-            // Check results 
-            // Tests passes/fails
             Assert.Equal(4, sum);
+        }
+
+        [Fact(Skip ="Don't run along the other tests")]
+        public void TestSum4And4() {
+            // Arrange Phase
+            //Calculator c = new();
+
+            // Act Phase
+            var sum = c.Sum(4, 4);
+
+            // Assert Phase 
+            Assert.Equal(8, sum);
+        }
+
+        [Fact]
+        public void TestSum6And6() {
+
+            _output.WriteLine("Logging TestSum6And6 run");
+
+            // Arrange Phase
+            //Calculator c = new();
+
+            // Act Phase
+            var sum = c.Sum(6, 6);
+
+            // Assert Phase 
+            Assert.Equal(12, sum);
         }
     }
 }
